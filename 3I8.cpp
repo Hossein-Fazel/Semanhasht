908470ddef99e234361e7a13129fe9363fe6f85c
medisa <medisakhabazi@gmail.com>
2024-02-04 11:30:35 +0330

    file fixed

--- 3I8.cpp
+++ 3I8.cpp
@@ -24,69 +24,6 @@ string Tehran::search(int key)
     }
 }
 
-void Tehran::add_station()
-{
-    stations.insert({"Kouhsar", 0});
-    stations.insert({"Kashani", 1});
-    stations.insert({"Yadegar-e Emam", 2});
-    stations.insert({"Boostan-e laleh", 3});
-    stations.insert({"Meydan-e Hazrat-e ValiAsr", 4});
-    stations.insert({"Haftom-e Tir", 5});
-    stations.insert({"Emam Hossein", 6});
-    stations.insert({"Meydan-e Shohada", 7});
-    stations.insert({"Shahid Rezaei", 8});
-    stations.insert({"Haram-e Hazrat-e Abdolazim", 9});
-    stations.insert({"Chaharbagh", 10});
-    stations.insert({"Allameh Jafari", 11});
-    stations.insert({"Eram-e Sabz", 12});
-    stations.insert({"Meydan-e Azadi", 13});
-    stations.insert({"Ostad Mo'in", 14});
-    stations.insert({"Shademan", 15});
-    stations.insert({"Towhid", 16});
-    stations.insert({"Meydan-e Enghelab-e Eslami", 17});
-    stations.insert({"Teatr-e Shahr", 18});
-    stations.insert({"Ferdowsi", 19});
-    stations.insert({"Darvazeh Dowlat", 20});
-    stations.insert({"Darvazeh Shemiran", 21});
-    stations.insert({"Ebn-e Sina", 22});
-    stations.insert({"Pirouzi", 23});
-    stations.insert({"Nabard", 24});
-    stations.insert({"Nirou Havaei", 25});
-    stations.insert({"Shahid Kolahdouz", 26});
-    stations.insert({"Azadegan", 27});
-    stations.insert({"Zamzam", 28});
-    stations.insert({"Javadiyeh", 29});
-    stations.insert({"Rahahan", 30});
-    stations.insert({"Mahdiyeh", 31});
-    stations.insert({"Moniriyeh", 32});
-    stations.insert({"Meydan-e Jahad", 33});
-    stations.insert({"Merza-ye Shirazi", 34});
-    stations.insert({"Shahid Beheshti", 35});
-    stations.insert({"Sohrevardi", 36});
-    stations.insert({"Shahid Ghodousi", 37});
-    stations.insert({"Shahid Zeynoddin", 38});
-    stations.insert({"Aghdasiyeh", 39});
-    stations.insert({"Gha'em", 40});
-    stations.insert({"Tajrish", 41});
-    stations.insert({"Gheytariyeh", 42});
-    stations.insert({"Gholhak", 43});
-    stations.insert({"Mirdamad", 44});
-    stations.insert({"Shahid Haghani", 45});
-    stations.insert({"Shahid Mofatteh", 46});
-    stations.insert({"Taleghani", 47});
-    stations.insert({"Panzdah-e Khordad", 48});
-    stations.insert({"Shoush", 49});
-    stations.insert({"Jonoub Terminal", 50});
-    stations.insert({"Shahr-e Rey", 51});
-    stations.insert({"Kahrizak", 52});
-    stations.insert({"Bimeh", 53});
-    stations.insert({"Tehran Pars", 54});
-    stations.insert({"Khajeh Abdollah-e Ansari", 55});
-    stations.insert({"Payaneh Javanmard", 56});
-    stations.insert({"Shahid Sadr", 57});
-    stations.insert({"Shahrak-e Shari'ati", 58});
-}
-
 int Tehran::minDistance(save_directions dist[], bool sptSet[])
 {
 
@@ -171,7 +108,6 @@ int Tehran::get_value(string key)
     {
         return -1;
     }
-    //cout << "val"<<stations[key];
     return stations[key];
 }
 
@@ -212,7 +148,6 @@ void Tehran::read_file()
             string line ;
             getline( file , line);
             line.erase(line.size()-1 , 1) ;
-            //cout << "line:" << line << endl ;
 
             while (!file.eof())
             {
@@ -228,25 +163,21 @@ void Tehran::read_file()
                         {
                             line.erase(line.size()-1 , 1) ;
                         }
-                        //cout << "line:" << line << endl ; 
                         getline(file , stat1) ;
                         
                 }
 
                 getline(file , stat2);
                 getline(file , dis);
-                //cout << "   :" << stat1 << " " << stat2 << " " << stoi(dis) << endl ; 
                 if (stations.count(stat1)== 0)
                 {
                     stations.insert({stat1 , index}) ;
-                    //cout << "state1 ;"<< stat1 << index << endl ;
                     index++ ;
                     
                 }
                 if (stations.count(stat2)== 0)
                 {
                     stations.insert({stat2 , index}) ;
-                    //cout << "state2 ;"<< stat2 << index << endl ;
                     index++ ;
                 }
                 

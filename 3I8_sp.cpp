ad171efb5bdc206f36c4210a08d2d327a0f7aea0
acontius <152691439+acontius@users.noreply.github.com>
2024-02-05 21:44:50 +0330

    Adding Line map.

--- 3I8_sp.cpp
+++ 3I8_sp.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <map>
 #include <fstream>
-
+#include <algorithm>
 using namespace std;
 
 #define V 59
@@ -107,29 +107,6 @@ int Tehran::get_value(string key)
     return stations[key];
 }
 
-void Tehran::create_busline()
-{
-    Busline1.insert({"Tehran Pars","Ebn-e Sina","Emam Hossein","Darvazeh Dowlat","Ferdowsi","Ferdowsi","Ostad Mo'in","Meydan-e Azadi","Bimeh"});
-    Busline2.insert({"Khajeh Abdollah-e Ansari","Gha'em","Pirouzi","Shahid Rezaei","Payaneh Javanmard"});
-    Busline3.insert({"Shahid Sadr","Tajrish","Mirdamad","Meydan-e Hazrat-e ValiAsr","Rahahan","Shahrak-e Shari'ati"});
-}
-
-void Tehran::create_metrolines()
-{
-    l4.insert({"Shahid Kolahdouz ", "Nirou Havaei", "Nabard", "Pirouzi", "Ebn-e Sina", "Meydan-e Shohada", "Darvazeh Shemiran",
-     "Darvazeh Dowlat", "Ferdowsi", "Teatr-e Shahr", "Meydan-e Enghelab-e Eslami", "Towhid", "Shademan", "Ostad Mo'in",
-      "Meydan-e Azadi", "Eram-e Sabz", "Allameh Jafari", "Kashani", "Chaharbagh"});
-
-    l3.insert({"Gha'em", "Aghdasiyeh", "Shahid Zeynoddin", "Shahid Ghodousi", "Sohrevardi", "Shahid Beheshti", "Merza-ye Shirazi",
-    "Meydan-e Jahad", "Meydan-e Hazrat-e ValiAsr", "Teatr-e Shahr", "Moniriyeh", "Mahdiyeh", "Rahahan", "Javadiyeh", "Zamzam", "Azadegan"});
-
-    l1.insert({"Tajrish", "Gheytariyeh", "Gholhak", "Mirdamad", "Shahid Haghani", "Shahid Beheshti", "Shahid Mofatteh", "Haftom-e Tir", "Taleghani",
-    "Darvazeh Dowlat", "Panzdah-e Khordad", "Shoush", "Jonoub Terminal", "Shahr-e Rey", "Shahr-e Rey"});
-
-    l6.insert({"Haram-e Hazrat-e Abdolazim", "Shahid Rezaei", "Meydan-e Shohada", "Emam Hossein", "Haftom-e Tir", "Meydan-e Hazrat-e ValiAsr",
-    "Boostan-e laleh", "Yadegar-e Emam", "Kashani", "Kouhsar"});
-}
-
 void Tehran::read_file()
 {
 
@@ -165,6 +142,16 @@ void Tehran::read_file()
 
                 getline(file , stat2);
                 getline(file , dis);
+                if(find(Linemap[line].begin(),Linemap[line].end(),stat1) == Linemap[line].end())
+                {
+                    Linemap[line].push_back(stat1);
+
+                }
+                if(find(Linemap[line].begin(),Linemap[line].end(),stat2) == Linemap[line].end())
+                {
+                    Linemap[line].push_back(stat2);
+                    
+                }
                 if (stations.count(stat1)== 0)
                 {
                     stations.insert({stat1 , index}) ;
--- 3I8_sp.hpp
+++ 3I8_sp.hpp
@@ -5,7 +5,6 @@
 #include<unordered_map>
 #include<vector>
 #include<limits.h>
-#include<unordered_set>
 
 using namespace std;
 
@@ -33,13 +32,10 @@ public:
     void Find_Shortest_Path(int src,int dest);
     string search(int key);
     int get_value(string key);
-    void create_busline();
-    void create_metrolines();
     void read_file();
 
 private:
-
-    unordered_set<string> Busline1, Busline2, Busline3, l4, l3, l1, l6;
+    unordered_map<string,vector<string>>Linemap;
     unordered_map<string, int> stations;
     Node_sp matrix[59][59];
 };

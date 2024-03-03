3a00f374211c5a4bb723cf0fb44dbb67b8ab803d
acontius <152691439+acontius@users.noreply.github.com>
2024-03-03 14:21:22 +0330

    Fixing the UI bug in Scale 100

---
+++ QT/form.h
@@ -0,0 +1,24 @@
+#ifndef FORM_H
+#define FORM_H
+
+#include <QWidget>
+
+namespace Ui {
+class Form;
+}
+
+class Form : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Form(QWidget *parent = nullptr);
+    ~Form();
+
+private slots:
+
+private:
+    Ui::Form *ui;
+};
+
+#endif // FORM_H
